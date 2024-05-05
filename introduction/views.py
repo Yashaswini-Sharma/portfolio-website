45a671707ac53a7cd554d79ed3369d03ee47f2f4
Yashaswini-sharma <yashaswini.sharma.ug23@nsut.ac.in>
2024-05-05 11:19:04 +0530

    website till hobbies

---
+++ introduction/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def index(request):
+    
+    return render(request, "introduction/index.html")
+
+
+def aboutMe(request):
+    
+    return render(request, "introduction/AboutMe.html")
+
+
